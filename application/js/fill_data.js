9816f7f8d39d4f58d91d636fb6519f488b3a8e1a
Carlos Espino <carlosespino11@gmail.com>
2015-12-08 21:08:18 -0500

    Working reviews and rank  on app

--- application/js/fill_data.js
+++ application/js/fill_data.js
@@ -30,6 +30,7 @@ $(function() {
       .append("a")
         .attr("class", "list-group-item hotel_card")
         .attr("id", function(d){return("hotel_card_" + d["id"])})
+        .attr("data-id", function(d){return(d["id"])})
         .attr("href", "#")
       .append("div")
         .attr("class", "row")
@@ -62,29 +63,74 @@ $(function() {
 
     $("#hotel_card_"+ hotel_active).addClass("active");
   }
+
   fill_reviews = function(data,hotel_id){
     reviews = data[hotel_id]["reviews"]
-    var review_cards = d3.select("#hotels-list").selectAll("a")
-      .data(hotels).enter()
-      .append("a")
-        .attr("class", "list-group-item hotel_card")
-        .attr("id", function(d){return("hotel_card_" + d["id"])})
-        .attr("href", "#")
+    d3.select("#reviews-container").text("")
+    var review_card = d3.select("#reviews-container").selectAll("div")
+      .data(reviews).enter()
       .append("div")
-        .attr("class", "row")
+      .attr("id", function(d){return("review_card_" + d["id"])})
+      .attr("class", "media")
+     
+    var media_left = review_card.append("div")
+      .attr("class", "media-left")
 
+    media_left.append("img")
+      .attr("class", "media-object")
+      .attr("src", "./img/contact-outline.png")
+      .attr("width", "50")
+      .attr("height", "50")
+
+
+    var media_body = review_card.append("div")
+      .attr("class", "media-body")
+
+    var media_body_list = media_body.append("ul")
+      .attr("class", "list-inline")
+
+    media_body_list.append("li").append("h4").text(function(d){return(d["name"])})
+
+    media_body_list.append("li")
+      .append("img")
+      .attr("src", function(d){
+        return("./img/"+ d.stars +"_stars.svg.png")
+      })
+      .attr("height", 20)
+    
+    media_body_list.append("li")
+      .text(function(d){return(d["date"])})
+
+    media_body.append("p")
+      .text(function(d){return(d["review"])})
+
+    var media_bosy_right = review_card.append("div")
+      .attr("class", "media-right")
+      .append("dl")
+    media_bosy_right.append("dt").text("Type")
+    media_bosy_right.append("dd").text(function(d){return(d["type"])})
+
+    media_bosy_right.append("dt").text("Probability")
+    media_bosy_right.append("dd").text(function(d){return(d["probability"])})   
   }
 
+  fill_rank = function(data, hotel_id){
+    var rank = data[hotel_id]["rank"]
+    $("#ranking-content").text("Top "+ rank)
+  }
 
   fill_hotels(hotel_default_id);
-  
+  fill_reviews(data, hotel_default_id)
+  fill_rank(data, hotel_default_id)
+
   $(".hotel_card").click(function(e){
     $(".hotel_card").removeClass("active")
     $(this).addClass("active")
-    
+    id = $(this).data("id")
+    fill_reviews(data, id)
+    fill_rank(data, id)
+    e.preventDefault()
   });
 
 })
-
-
 
